84a02ff1dc0f1ab97832cfc87e5946db15f3774c
guillaume1060 <breyerguillaume@gmail.com>
2022-06-22 16:35:55 +0200

    update

--- javascript-data-visualisation/challenge-js-files/script.js
+++ javascript-data-visualisation/challenge-js-files/script.js
@@ -1,5 +1,7 @@
-
 /// Projet 2
+
+////Récupération des datas avec fetch
+
 const url = 'https://canvasjs.com/services/data/datapoints.php';
 
 async function recupererData() {
@@ -11,30 +13,39 @@ async function recupererData() {
     if(!requete.ok) {
       alert('Un problème est survenu.');
     } else {
-      let donnees = await requete.json();
-      console.log(donnees);
-      document.document.getElementById('firstHeading').textContent = donnees.last;
+      let donneesRecuperees = await requete.json();
+      console.log (donneesRecuperees);
     }
-  }
-  
+ 
+
+
+/// placement du graph
+var graph3 = document.createElement("canvas");
+graph3.setAttribute("id", "ajaxTable");
+document.querySelector('#firstHeading').prepend(graph3);
+
+
+var datasets3 = [{
+        data: [8,24,3],
+}];
+
+
+
+/// creation du graph
+const data3 = {
+    labels: ["x","y","z"],
+    datasets: datasets3,
+}
+
+var exo3 = new Chart (graph3,
+//const config = { ici on configure le tableau
+    {
+        type:'bar',
+        data: data3,
+    })
+
+
+}
+
    recupererData();
-
-   /// placemdent du graph
-    var graph3 = document.createElement("canvas");
-    graph3.setAttribute("id", "ajaxTable");
-    document.querySelector('#firstHeading').prepend(graph3);
-
-
-
-    /// creation du graph
-    const data = {
-        labels: years,
-        datasets: datasets,
-    }
-
-    var test = new Chart (canvas1,
-    //const config = { ici on configure le tableau
-        {
-            type:'line',
-            data: data,
-        })
+   
