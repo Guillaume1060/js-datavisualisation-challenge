98ad712eb678b280a15b6ac5bdfe175e8f99d494
guillaume1060 <breyerguillaume@gmail.com>
2022-06-23 18:30:24 +0200

    update

--- javascript-data-visualisation/challenge-js-files/script.js
+++ javascript-data-visualisation/challenge-js-files/script.js
@@ -1,59 +1,79 @@
-/// Projet 2
+const url = "https://canvasjs.com/services/data/datapoints.php";
 
-////Récupération des datas avec fetch
+async function fetchMethod() {
+  const requete = await fetch(url, {
+    method: "GET",
+    cache: "reload",
+  });
 
-const url = 'https://canvasjs.com/services/data/datapoints.php';
+  if (!requete.ok) {
+    alert("Un problème est survenu.");
+  } else {
+    let donneesRecuperees = await requete.json();
+    var graph3 = document.createElement("canvas");
+    graph3.setAttribute("id", "ajaxTable");
+    document.querySelector("#firstHeading").prepend(graph3);
 
-
-async function recupererData() {
-
-    const requete = await fetch(url, {
-      method: 'GET',
-      cache : 'reload'
-    });
-    
-    if(!requete.ok) {
-      alert('Un problème est survenu.');
-    } else {
-        let donneesRecuperees = await requete.json();
-        // on intègre le graph ici pour récupérer les datas
-        /// placement du graph
-        var graph3 = document.createElement("canvas");
-        graph3.setAttribute("id", "ajaxTable");
-        document.querySelector('#firstHeading').prepend(graph3);
-
-        console.log (donneesRecuperees)
-
-      var x = [];
-      var y = [];
-      for (i=0;i<donneesRecuperees.length;i++)
-      {
-        x.push(donneesRecuperees[i][0])
-        y.push(donneesRecuperees[i][1])
-      }
-
-
-        var datasets3 = [{
-                data: y
-        }];
-
-
-
-        /// creation du graph
-        const data3 = {
-            labels: x,
-            datasets: datasets3,
-        }
-
-        var exo3 = new Chart (graph3,
-        //const config = { ici on configure le tableau
-            {
-                type:'line',
-                data: data3,
-            })
+    var x = [];
+    var y = [];
+    for (i = 0; i < donneesRecuperees.length; i++) {
+      x.push(donneesRecuperees[i][0]);
+      y.push(donneesRecuperees[i][1]);
     }
+
+    let data3 = {
+      labels: x,
+      datasets: [{ data: y }],
+    };
+
+    var exo3 = new Chart(
+      graph3,
+      {
+        type: "line",
+        data: data3,
+      }
+    );
+  }
+
+
+
+
+  function addData(chart, data, datasetIndex) {
+    chart.data.datasets[datasetIndex].data = data;
+    chart.update();
+  }
+
+  setInterval(function () {
+    addData(exo3,x, 0);
+  }, 1000);
+}
+
+fetchMethod();
+
+function update (donnees)
+{
+  console.log (donnees)
 }
 
 
-// setInterval(UpdateData,1000);
-recupererData();
+
+// function refresh() {
+//   $.ajax({
+//       url: 'https://canvasjs.com/services/data/datapoints.php', // Ton fichier ou se trouve ta data
+//       success:
+//           function recupererData(){}
+//   });
+//   }
+
+//   function addData(chart, data, datasetIndex) {
+//         chart.data.datasets[datasetIndex].data = data;
+//         console.log (y)
+//         chart.update();
+// }
+
+// setInterval(function(){
+//   addData(exo3,y,0)
+// },1000);
+// setInterval();
+
+
