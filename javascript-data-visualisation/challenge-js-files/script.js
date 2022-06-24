32e25d5613ad7e65aa5e4d01819d29ac18851fe3
guillaume1060 <breyerguillaume@gmail.com>
2022-06-24 10:34:03 +0200

    final version

--- javascript-data-visualisation/challenge-js-files/script.js
+++ javascript-data-visualisation/challenge-js-files/script.js
@@ -1,6 +1,48 @@
-const url = "https://canvasjs.com/services/data/datapoints.php";
+/// DISPLAY OF THE GRAPHIC WITHOUT DATAS
+var graph3 = document.createElement("canvas");
+graph3.setAttribute("id", "ajaxTable");
+document.querySelector("#firstHeading").prepend(graph3);
 
+var x = [];
+var y = []
+let data3 = {
+  labels: x,
+  datasets: [{  data: y,
+                label: null,
+                borderColor : 'red',
+                backgroundColor : 'red' 
+            }],
+};
+
+var exo3 = new Chart(graph3, {
+  type: "line",
+  data: data3,
+  options: {
+    plugins: {
+        legend: {
+          display: false}
+}}});
+
+/// UPDATE THE GRAPH WITH FETCH DATAS AS VARIABLE
+function update(donneesRecuperees) {
+  for (i = 0; i < donneesRecuperees.length; i++) {
+    x.push(donneesRecuperees[i][0]);
+    y.push(donneesRecuperees[i][1]);
+  }
+
+  function addData(chart, labels, dataY, datasetIndex) {
+    chart.data.datasets[datasetIndex].data = dataY;
+    chart.data.labels = labels;
+    chart.update();
+  }
+
+  addData(exo3, x, y, 0);
+}
+
+
+/// ASYNC FUNCTION TO GET URL DATAS and CALL THE UPDATE FUNCTION
 async function fetchMethod() {
+  const url = "https://canvasjs.com/services/data/datapoints.php";
   const requete = await fetch(url, {
     method: "GET",
     cache: "reload",
@@ -10,70 +52,8 @@ async function fetchMethod() {
     alert("Un problème est survenu.");
   } else {
     let donneesRecuperees = await requete.json();
-    var graph3 = document.createElement("canvas");
-    graph3.setAttribute("id", "ajaxTable");
-    document.querySelector("#firstHeading").prepend(graph3);
-
-    var x = [];
-    var y = [];
-    for (i = 0; i < donneesRecuperees.length; i++) {
-      x.push(donneesRecuperees[i][0]);
-      y.push(donneesRecuperees[i][1]);
-    }
-
-    let data3 = {
-      labels: x,
-      datasets: [{ data: y }],
-    };
-
-    var exo3 = new Chart(
-      graph3,
-      {
-        type: "line",
-        data: data3,
-      }
-    );
+    update(donneesRecuperees);
   }
-
-
-
-
-  function addData(chart, data, datasetIndex) {
-    chart.data.datasets[datasetIndex].data = data;
-    chart.update();
-  }
-
-  setInterval(function () {
-    addData(exo3,x, 0);
-  }, 1000);
 }
 
-fetchMethod();
-
-function update (donnees)
-{
-  console.log (donnees)
-}
-
-
-
-// function refresh() {
-//   $.ajax({
-//       url: 'https://canvasjs.com/services/data/datapoints.php', // Ton fichier ou se trouve ta data
-//       success:
-//           function recupererData(){}
-//   });
-//   }
-
-//   function addData(chart, data, datasetIndex) {
-//         chart.data.datasets[datasetIndex].data = data;
-//         console.log (y)
-//         chart.update();
-// }
-
-// setInterval(function(){
-//   addData(exo3,y,0)
-// },1000);
-// setInterval();
-
-
+setInterval(fetchMethod, 1000);
